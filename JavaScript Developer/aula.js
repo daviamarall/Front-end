8ce24973b6be6ff8bb0f6f21c03a1051c94f7cc9
Davi Amaral <40430859+daviamarall@users.noreply.github.com>
2025-05-29 17:14:37 +0000

    update

---
+++ JavaScript Developer/aula.js
@@ -0,0 +1,9 @@
+
+const precoCombustivel = 3.70;
+let gastoPorKM = 10;
+let distancia = 500; 
+
+let litrosConsumidos = distancia / gastoPorKM ;
+let valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(valorGasto)
